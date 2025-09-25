b053c91e86769fba3e7839ce649f10300b25ce3e
YuFuru513 <212640630+YuFuru513@users.noreply.github.com>
2025-09-25 10:27:50 +0900

    リファクタ: ディレクトリ名とプロジェクト名の統一、認証サービスAPIの一貫性向上
    - プロジェクト名修正時にディレクトリ名修正漏れていたので修正
    - サインアウト時もデフォルト値指定可能に変更

---
+++ 01-basic-mfa-csharp/BasicMfaSample.Services/Interfaces/IEntraAuthService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace BasicMfaSample.Services.Interfaces
+{
+    public interface IEntraAuthService
+    {
+        /// <summary>
+        /// サインイン時の認証プロパティを生成する
+        /// </summary>
+        /// <param name="returnUrl">認証完了後の戻り先</param>
+        /// <returns>認証プロパティ</returns>
+        AuthenticationProperties CreateSignInProperties(string returnUrl = "/Dashboard/Index");
+
+        /// <summary>
+        /// サインアウト時の認証プロパティを生成する
+        /// </summary>
+        /// <param name="returnUrl">認証完了後の戻り先</param>
+        /// <returns>認証プロパティ</returns>
+        AuthenticationProperties CreateSignOutProperties(string returnUrl = "/");
+    }
+}
